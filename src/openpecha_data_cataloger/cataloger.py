a45c07b4e9cd78b113639531e478d1e69d3431d4
Tenzin <tenzintsunduebhattu@gmail.com>
2024-01-08 16:06:36 +0530

    load_pechas_using_toolkit_OpenPechaGitRepo_class

--- src/openpecha_data_cataloger/cataloger.py
+++ src/openpecha_data_cataloger/cataloger.py
@@ -1,6 +1,8 @@
 import csv
 
-from openpecha_data_cataloger.config import CATALOG_DIR
+from openpecha.core.pecha import OpenPechaGitRepo
+
+from openpecha_data_cataloger.config import CATALOG_DIR, set_environment
 from openpecha_data_cataloger.github_token import GITHUB_TOKEN
 from openpecha_data_cataloger.utility import download_github_file
 
@@ -15,22 +17,36 @@ class Cataloger:
         self.catalog_path = CATALOG_DIR / self.file_name
         self.get_catalog()
         self.catalog = self.load_catalog()
+        self.pecha_ids = self.get_catalog_pecha_names()
+        set_environment()
 
     def get_catalog(self):
-        download_github_file(
-            token=self.token,
-            org=self.org,
-            repo_name=self.repo,
-            file=self.file_name,
-            destination_folder_path=self.catalog_path.parent,
-        )
+        if not self.catalog_path.exists():
+            download_github_file(
+                token=self.token,
+                org=self.org,
+                repo_name=self.repo,
+                file=self.file_name,
+                destination_folder_path=self.catalog_path.parent,
+            )
 
     def load_catalog(self):
         with open(self.catalog_path, newline="") as csvfile:
             catalog = csv.DictReader(csvfile)
             return list(catalog)
 
+    def get_catalog_pecha_names(self):
+        return [row["Pecha ID"] for row in self.catalog]
+
+    def load_pechas(self, pecha_ids=None):
+        if pecha_ids is None:
+            pecha_ids = self.pecha_ids
+        self.pechas = (self.load_pecha(pecha_id) for pecha_id in pecha_ids)
+
+    def load_pecha(self, pecha_id):
+        return OpenPechaGitRepo(pecha_id=pecha_id)
+
 
 if __name__ == "__main__":
-    cataloger = Cataloger(token=GITHUB_TOKEN)
-    print(cataloger.catalog)
+    cataloger = Cataloger(GITHUB_TOKEN)
+    cataloger.load_pechas(["P000216", "P000217"])
--- src/openpecha_data_cataloger/config.py
+++ src/openpecha_data_cataloger/config.py
@@ -1,4 +1,7 @@
+import os
 from pathlib import Path
+
+from openpecha_data_cataloger.github_token import GITHUB_TOKEN
 
 
 def _mkdir(path):
@@ -6,6 +9,12 @@ def _mkdir(path):
         return path
     path.mkdir(exist_ok=True, parents=True)
     return path
+
+
+def set_environment():
+    os.environ["GITHUB_TOKEN"] = GITHUB_TOKEN
+    os.environ["OPENPECHA_DATA_GITHUB_ORG"] = "OpenPecha-Data"
+    os.environ["GITHUB_USERNAME"] = "gangagyatso4364"
 
 
 ROOT_DIR = Path(__file__).parent.parent.parent
