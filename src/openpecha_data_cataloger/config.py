8cc6229f0b703a130c40fbe11c8b8945daaa8a08
Tenzin <tenzintsunduebhattu@gmail.com>
2024-01-10 14:45:40 +0530

    modify_take_GITHUB_TOKEN_from_env_in_config

--- src/openpecha_data_cataloger/config.py
+++ src/openpecha_data_cataloger/config.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-from openpecha_data_cataloger.github_token import GITHUB_TOKEN
-
 
 def _mkdir(path):
     if path.is_dir():
@@ -12,7 +10,8 @@ def _mkdir(path):
 
 
 def set_environment():
-    os.environ["GITHUB_TOKEN"] = GITHUB_TOKEN
+    """fetch github token from environment variable"""
+    os.environ["GITHUB_TOKEN"] = os.environ.get("GITHUB_TOKEN", "your_default_token")
     os.environ["OPENPECHA_DATA_GITHUB_ORG"] = "OpenPecha-Data"
     os.environ["GITHUB_USERNAME"] = "tenzin3"
 
