2f66e694001319ec186ec1cde23eda41537341a8
Tenzin <tenzintsunduebhattu@gmail.com>
2024-01-12 16:46:02 +0530

    update_test_case_for_annotation_content_report

--- tests/test_annotation_content_report_generator.py
+++ tests/test_annotation_content_report_generator.py
@@ -5,7 +5,7 @@ from openpecha_data_cataloger.cataloger import Cataloger
 
 def test_annotation_content_report_generator():
     cataloger = Cataloger()
-    cataloger.load_pechas(["P000216", "I1A92E2D9", "O869F9D37"], path=PECHAS_DIR)
+    cataloger.load_pechas(["P000216", "I1A92E2D9"], path=PECHAS_DIR)
 
     catalog_df = cataloger.generate_annotation_content_report()
 
@@ -32,16 +32,33 @@ def test_annotation_content_report_generator():
 
     # Define the expected values for each test case
     expected_yigchung = {
+        "pecha_id": "P000216",
+        "volume_name": "v001",
         "has_base_file": True,
-        "is_annotation_type_enumed": True,
+        "annotation_file_name": "Yigchung",
         "is_annotation_file_name_enumed": True,
+        "base_fields": ["id", "annotation_type", "revision", "annotations"],
+        "undefined_base_fields": [],
+        "has_annotation_type": True,
+        "annotation_type": "Yigchung",
+        "is_annotation_type_enumed": True,
+        "has_annotations": True,
+        "has_annotation_id_missing": False,
+        "annotation_fields": ["span"],
+        "has_annotation_field_validation_error": False,
+        "required_annotation_fields": ["span"],
+        "missing_annotation_fields": [],
+        "extra_annotation_fields": [],
         "has_span_annotation": True,
-        "start_end_greater_than_base_file_length": {},
+        "has_start_end_in_span": True,
+        "start_end_are_integer": {},
         "start_greater_equal_than_end": {
             "3e92bdc162b444fe96f12703fa880f0a": {
                 "span": {"start": 189353, "end": 189353}
             }
         },
+        "start_end_negative_values": {},
+        "start_end_greater_than_base_file_length": {},
     }
 
     expected_author = {
@@ -57,8 +74,15 @@ def test_annotation_content_report_generator():
         "is_annotation_type_enumed": False,
         "has_annotations": True,
     }
+    expected_segment = {
+        "volume_name": "ABFB",
+        "annotation_type": "Segment",
+        "is_annotation_type_enumed": True,
+        "has_annotation_id_missing": False,
+    }
 
     # Run the tests
     test_specific_annotation("P000216", "v001", "Yigchung", expected_yigchung)
     test_specific_annotation("P000216", "v001", "Author", expected_author)
     test_specific_annotation("P000216", "v001", "Quotation", expected_quotation)
+    test_specific_annotation("I1A92E2D9", "ABFB", "Segment", expected_segment)
